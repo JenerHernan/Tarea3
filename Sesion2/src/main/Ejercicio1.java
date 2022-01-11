bb633e22d09f7f1c58763e574de1e024fe7a3662
Jener Hernan <74436720+JenerHernan@users.noreply.github.com>
2022-01-11 13:00:51 +0100

    Commit con todos los ejercicios de la tarea 3

---
+++ Sesion2/src/main/Ejercicio1.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		int num1 = 20;
+		int num2 = 10;
+		System.out.println("La suma de las variables es: " + (num1 + num2));
+		System.out.println("La resta de las variables es: " + (num1 - num2));
+		System.out.println("La multiplicacion de las variables es: " + (num1 * num2));
+		System.out.println("La division de las variables es: " + (num1 / num2));
+		
+	}
+
+}
---
+++ Sesion2/src/main/Ejercicio4.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		n += 77;
+		n -= 3;
+		n *= 2;
+		
+		System.out.print(n);
+		
+	}
+
+}
